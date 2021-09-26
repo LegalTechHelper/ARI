acbcf2d51e637bc21189e673dde0ff7e262a7c23
LegalTechHelper <info@legaltechhelper.com>
2021-09-26 11:32:40 +1000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cp -r /tmp/ARI/packages/docassemble-ARI /tmp
-source /usr/share/docassemble/local3.8/bin/activate && pip3 install --upgrade /tmp/docassemble-ARI
+source /usr/share/docassemble/local3.8/bin/activate && pip3 install --upgrade /tmp/ARI
 echo "Package docassemble-ARI installed and is available on"
 echo "https://<your domain>/interview?i=docassemble.ARI:data/questions/text.yml"
